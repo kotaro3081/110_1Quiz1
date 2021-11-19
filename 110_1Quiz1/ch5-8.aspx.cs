35264c1c30ff6a9fe7d41485feda61fe5fa4f748
宏儒 林 <108111123@mail.aeust.edu.tw>
2021-11-19 10:50:47 +0800

    Ad rotation

---
+++ 110_1Quiz1/ch5-8.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Quiz1
+{
+    public partial class ch5_8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
